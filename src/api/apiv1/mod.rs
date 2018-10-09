428cf3d464e7f9663d8c1d2eed30cd2cdbb24076
Tao Yang <yangtao3601@126.com>
2018-10-09 10:59:32 +0800

    fix

--- src/api/apiv1/mod.rs
+++ src/api/apiv1/mod.rs
@@ -5,7 +5,6 @@ fn hello() -> String {
 	format!("hello world!")
 }
 
-pub fn registerAPIv1(r: rocket::Rocket) -> rocket::Rocket{
+pub fn registerAPIv1(r: &rocket::Rocket) {
 	r.mount("/", routes![hello]);
-	r
 }
--- src/api/mod.rs
+++ src/api/mod.rs
@@ -11,9 +11,8 @@ impl apiServer {
 		self.r.launch();
 	}
 
-	pub fn configServerHandler(self) -> Self {
-		self.r = apiv1::registerAPIv1(self.r)
-		self
+	pub fn configServerHandler(self) {
+		apiv1::registerAPIv1(&self.r);
 	}
 }
 
