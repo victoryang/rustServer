9e0c818c007852b7d9a84a05b7759e9284cae174
Tao Yang <yangtao3601@126.com>
2018-10-29 16:43:23 +0800

    add mcsql library

---
+++ mcsql-sys/build.rs
@@ -0,0 +1,57 @@
+extern crate flate2;
+extern crate tar;
+
+use std::process::Command;
+use std::path::{PathBuf};
+use std::env;
+use std::fs;
+
+use std::fs::File;
+use flate2::read::GzDecoder;
+use tar::Archive;
+
+fn decompress(src: &str, des: &str) -> Result<(), std::io::Error> {
+	let tar_gz = File::open(src)?;
+	let tar = GzDecoder::new(tar_gz);
+	let mut archive = Archive::new(tar);
+	archive.unpack(des)?;
+
+	Ok(())
+}
+
+fn main() {
+	let root = PathBuf::from(env::var_os("OUT_DIR").unwrap());
+
+    let mcsql = dst.join("mcsql");
+    let include = mcsql.join("include");
+    let libdir = mcsql.join("lib");
+
+    println!("cargo:root={}", root.display());
+    println!("cargo:include={}", include.display());
+    println!("cargo:libdir={}", libdir.display());
+    //println!("cargo:static=1");
+
+    match decompress("build/include/include.tar.gz", format!("{}", mcsql.display()).as_str()) {
+    	Ok(()) => {
+    		fs::copy("build/include/config.h", mcsql.join("config.h")).unwrap();
+    		fs::copy("mcsql/define.h", include.join("define.h")).unwrap();
+		    fs::copy("mcsql/mcmanager.h", include.join("mcmanager.h")).unwrap();
+		    fs::copy("mcsql/mcquery.h", include.join("mcquery.h")).unwrap();
+		    fs::copy("mcsql/mcsql.h", include.join("mcsql.h")).unwrap();
+		    fs::copy("mcsql/mcsqlmapper.h", include.join("mcsqlmapper.h")).unwrap();
+    	},
+    	Err(_) => return,
+    }
+
+    fs::create_dir_all(&libdir).unwrap();
+    fs::copy("build/lib/libz.so.1.2.8", libdir.join("libz.so.1.2.8")).unwrap();
+    fs::copy("build/lib/libsqlitedb.so", libdir.join("libsqlitedb.so")).unwrap();
+
+    Command::new("make").args(&["-C", "mcsql/"]).status().unwrap();
+    fs::copy("mrj/libmcsql.so", libdir.join("libmcsql.so")).unwrap();
+
+    println!("cargo:rustc-link-lib=z");
+    println!("cargo:rustc-link-lib=sqlitedb");
+    println!("cargo:rustc-link-lib=dylib=mcsql");
+    println!("cargo:rustc-link-search=native={}", libdir.display());
+}
