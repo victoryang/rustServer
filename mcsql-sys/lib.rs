7895b23d1b5aaf520f78b822fa6427f467da902f
Tao Yang <yangtao3601@126.com>
2018-10-31 17:25:32 +0800

    fix

--- mcsql-sys/lib.rs
+++ mcsql-sys/lib.rs
@@ -2,18 +2,15 @@
 extern crate libc;
 
 use libc::{c_char, c_int};
+use std::ffi::CString;
 
 extern {fn register_all_sql_mappers();};
-extern {fn mcsql_set_db_file(dbname: *mut c_char);};
-
-/*
-* Sqlite3 Connection
-*/
-pub const conn = b'/rbctrl/db/elibotDB.db';
+extern {fn mcsql_set_db_file(dbname: *const c_char);};
 
 pub fn init() {
+	let conn = CString::new(conn).unwrap();
 	unsafe {
 		register_all_sql_mappers();
-		mcsql_set_db_file(conn);
+		mcsql_set_db_file(conn.as_ptr());
 	}
 }
