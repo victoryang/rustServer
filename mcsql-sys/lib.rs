98e658c1f7ea2205e1de8a63ceb224e856c63186
Tao Yang <yangtao3601@126.com>
2018-10-31 17:26:57 +0800

    fix

--- mcsql-sys/lib.rs
+++ mcsql-sys/lib.rs
@@ -4,8 +4,8 @@ extern crate libc;
 use libc::{c_char, c_int};
 use std::ffi::CString;
 
-extern {fn register_all_sql_mappers();};
-extern {fn mcsql_set_db_file(dbname: *const c_char);};
+extern {fn register_all_sql_mappers();}
+extern {fn mcsql_set_db_file(dbname: *const c_char);}
 
 pub fn init() {
 	let conn = CString::new(conn).unwrap();
