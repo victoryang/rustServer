b27d624c2814a574b2655b9bee415f4e7d225cf9
Tao Yang <yangtao3601@126.com>
2018-11-01 15:47:26 +0800

    fix

--- mcsql-sys/build.rs
+++ mcsql-sys/build.rs
@@ -45,9 +45,8 @@ fn main() {
     fs::copy("build/lib/libsqlitedb.so", libdir.join("libsqlitedb.so")).unwrap();
 
     Command::new("make").args(&["-C", "mcsql/"]).status().unwrap();
-    fs::copy("mcsql/libmcsql.a", libdir.join("libmcsql.a")).unwrap();
+    fs::copy("mcsql/libmcsql.so", libdir.join("libmcsql.so")).unwrap();
 
     println!("cargo:rustc-link-lib=sqlitedb");
-    println!("cargo:rustc-link-lib=static=mcsql");
     println!("cargo:rustc-link-search=native={}", libdir.display());
 }
