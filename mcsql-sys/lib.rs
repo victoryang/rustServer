b7f8d389274a215baabafd2bb5733a8a9344247f
Tao Yang <yangtao3601@126.com>
2018-11-07 14:17:28 +0800

    apply all sqlitedb apis

--- mcsql-sys/lib.rs
+++ mcsql-sys/lib.rs
@@ -31,14 +31,25 @@ extern {fn mcsql_userframe_get_all() -> *mut c_char;}
 extern {fn mcsql_userframe_get_by_userno(user_no: int32_t) -> *mut c_char;}
 extern {fn mcsql_zeropoint_get_all() -> *mut c_char;}
 
-pub fn arc_get_all() -> String {
-	let c_string = unsafe { CString::from_raw(mcsql_arc_get_all()) };
+// Turn C result into String, responded to caller
+fn result_into_string_response(c_result: *mut c_char) -> String {
+	if c_result.is_null() {
+		return String::from("null");
+	}
+
+	let c_string = unsafe { CString::from_raw(c_result) };
 	match c_string.into_string() {
 		Ok(s) => {return s;},
 		Err(_) => {
-			return String::from("");
+			return String::from("fails to get result");
 		},
 	}
+}
+
+pub fn arc_get_all() -> String {
+	let c_result = unsafe { mcsql_arc_get_all(tool_no) };
+
+	result_into_string_response(c_result)
 }
 
 pub fn arc_get_params(file_no: i32, group: String) -> String {
@@ -49,53 +60,33 @@ pub fn arc_get_params(file_no: i32, group: String) -> String {
 		}
 	};
 
-	let c_string = unsafe { CString::from_raw(mcsql_arc_get_params(file_no, group.as_ptr())) };
-	match c_string.into_string() {
-		Ok(s) => {return s;},
-		Err(_) => {
-			return String::from("");
-		},
-	}
+	let c_result = unsafe { mcsql_arc_get_params(file_no, group.as_ptr()) };
+
+	result_into_string_response(c_result)
 }
 
 pub fn bookprogram_get_all() -> String {
-	let c_string = unsafe { CString::from_raw(mcsql_bookprogram_get_all()) };
-	match c_string.into_string() {
-		Ok(s) => {return s;},
-		Err(_) => {
-			return String::from("");
-		},
-	}
+	let c_result = unsafe { mcsql_bookprogram_get_all() };
+
+	result_into_string_response(c_result)
 }
 
 pub fn enum_get_all() -> String {
-	let c_string = unsafe { CString::from_raw(mcsql_enum_get_all()) };
-	match c_string.into_string() {
-		Ok(s) => {return s;},
-		Err(_) => {
-			return String::from("");
-		},
-	}
+	let c_result = unsafe { mcsql_enum_get_all() };
+
+	result_into_string_response(c_result)
 }
 
 pub fn extaxis_get_all() -> String {
-	let c_string = unsafe { CString::from_raw(mcsql_extaxis_get_all()) };
-	match c_string.into_string() {
-		Ok(s) => {return s;},
-		Err(_) => {
-			return String::from("");
-		},
-	}
+	let c_result = unsafe { mcsql_extaxis_get_all() };
+
+	result_into_string_response(c_result)
 }
 
 pub fn interference_get_all() -> String {
-	let c_string = unsafe { CString::from_raw(mcsql_interference_get_all()) };
-	match c_string.into_string() {
-		Ok(s) => {return s;},
-		Err(_) => {
-			return String::from("");
-		},
-	}
+	let c_result = unsafe { mcsql_interference_get_all() };
+
+	result_into_string_response(c_result)
 }
 
 pub fn ios_get_all(group: String, lang: String, auth: i32, tech: i32) -> String {
@@ -113,13 +104,9 @@ pub fn ios_get_all(group: String, lang: String, auth: i32, tech: i32) -> String 
 		}
 	};
 
-	let c_string = unsafe { CString::from_raw(mcsql_ios_get_all(group.as_ptr(), lang.as_ptr(), auth, tech)) };
-	match c_string.into_string() {
-		Ok(s) => {return s;},
-		Err(_) => {
-			return String::from("");
-		},
-	}
+	let c_result = unsafe { mcsql_ios_get_all(group.as_ptr(), lang.as_ptr(), auth, tech) };
+
+	result_into_string_response(c_result)
 }
 
 pub fn metadata_get_all(lang: String) -> String {
@@ -130,24 +117,15 @@ pub fn metadata_get_all(lang: String) -> String {
 		}
 	};
 
-	let c_string = unsafe { CString::from_raw(mcsql_metadata_get_all(lang.as_ptr())) };
-	match c_string.into_string() {
-		Ok(s) => {return s;},
-		Err(_) => {
-			return String::from("");
-		},
-	}
+	let c_result = unsafe { mcsql_metadata_get_all(lang.as_ptr()) };
+
+	result_into_string_response(c_result)
 }
 
 pub fn params_get_params() -> String {
-	let c_string = unsafe { CString::from_raw(mcsql_params_get_params()) };
+	let c_result = unsafe { mcsql_params_get_params() };
 
-	match c_string.into_string() {
-		Ok(s) => {return s;},
-		Err(_) => {
-			return String::from("");
-		},
-	}
+	result_into_string_response(c_result)
 }
 
 pub fn params_get_valid_param_by_id(md_id: String) -> String {
@@ -158,13 +136,9 @@ pub fn params_get_valid_param_by_id(md_id: String) -> String {
 		}
 	};
 
-	let c_string = unsafe { CString::from_raw(mcsql_params_get_valid_param_by_id(md_id.as_ptr())) };
-	match c_string.into_string() {
-		Ok(s) => {return s;},
-		Err(_) => {
-			return String::from("");
-		},
-	}
+	let c_result = unsafe { mcsql_params_get_valid_param_by_id(md_id.as_ptr()) };
+
+	result_into_string_response(c_result)
 }
 
 pub fn params_get_valid_param_by_group(group: String) -> String {
@@ -175,79 +149,45 @@ pub fn params_get_valid_param_by_group(group: String) -> String {
 		}
 	};
 
-	let c_string = unsafe { CString::from_raw(mcsql_params_get_valid_param_by_group(group.as_ptr())) };
-	match c_string.into_string() {
-		Ok(s) => {return s;},
-		Err(_) => {
-			return String::from("");
-		},
-	}
+	let c_result = unsafe { mcsql_params_get_valid_param_by_group(group.as_ptr()) };
+
+	result_into_string_response(c_result)
 }
 
 pub fn ref_get_all() -> String {
-	let c_string = unsafe { CString::from_raw(mcsql_ref_get_all()) };
+	let c_result = unsafe { mcsql_ref_get_all() };
 
-	match c_string.into_string() {
-		Ok(s) => {return s;},
-		Err(_) => {
-			return String::from("");
-		},
-	}
+	result_into_string_response(c_result)
 }
 
 pub fn toolframe_get_all() -> String {
-	let c_string = unsafe { CString::from_raw(mcsql_toolframe_get_all()) };
+	let c_result = unsafe { mcsql_toolframe_get_all() };
 
-	match c_string.into_string() {
-		Ok(s) => {return s;},
-		Err(_) => {
-			return String::from("");
-		},
-	}
+	result_into_string_response(c_result)
 }
 
 pub fn toolframe_get_by_toolno(tool_no: i32) -> String {
-	let c_string = unsafe { CString::from_raw(mcsql_toolframe_get_by_toolno(tool_no)) };
-
-	match c_string.into_string() {
-		Ok(s) => {return s;},
-		Err(_) => {
-			return String::from("");
-		},
-	}
+	let c_result = unsafe { mcsql_toolframe_get_by_toolno(tool_no) };
+	
+	result_into_string_response(c_result)
 }
 
 pub fn userframe_get_all() -> String {
-	let c_string = unsafe { CString::from_raw(mcsql_userframe_get_all()) };
+	let c_result = unsafe { mcsql_userframe_get_all() };
 
-	match c_string.into_string() {
-		Ok(s) => {return s;},
-		Err(_) => {
-			return String::from("");
-		},
-	}
+	result_into_string_response(c_result)
 }
 
 pub fn userframe_get_by_userno(user_no: i32) -> String {
-	let c_string = unsafe { CString::from_raw(mcsql_userframe_get_by_userno(user_no)) };
+	let c_result = unsafe { mcsql_userframe_get_by_userno(user_no) };
 
-	match c_string.into_string() {
-		Ok(s) => {return s;},
-		Err(_) => {
-			return String::from("");
-		},
-	}
+	result_into_string_response(c_result)
 }
 
 pub fn zeropoint_get_all() -> String {
-	let c_string = unsafe { CString::from_raw(mcsql_zeropoint_get_all()) };
+	let c_result = unsafe { mcsql_zeropoint_get_all() };
 
-	match c_string.into_string() {
-		Ok(s) => {return s;},
-		Err(_) => {
-			return String::from("");
-		},
-	}
+	result_into_string_response(c_result)
 }
 
 pub fn init() {
