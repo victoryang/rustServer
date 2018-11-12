1f20cd56790e243b859c27e0d1f6a50d86a92e78
Tao Yang <yangtao3601@126.com>
2018-11-12 14:39:55 +0800

    fix

--- src/rpc/ws.rs
+++ src/rpc/ws.rs
@@ -3,11 +3,11 @@ use jsonrpc_tcp_server::jsonrpc_core::*;
 
 pub fn register_websocket_funcs(io: &mut IoHandler, websocket: mpsc::Sender<Vec<u8>>) {
 	let send_to_websocket = move |message| -> bool {
-		match websocket.send(value.message){
-			Ok() => true,
-			Err() => false,
+		match websocket.send(message){
+			Ok(()) => true,
+			Err(_) => false,
 		}
-	}
+	};
 
 	io.add_method("alarm_record_changes", |params: Params| {
 		#[derive(Deserialize)]
