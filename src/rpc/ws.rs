4129d498307f52a7113bfe7151a52c650b9f6519
Tao Yang <yangtao3601@126.com>
2018-11-12 14:38:30 +0800

    fix

--- src/rpc/ws.rs
+++ src/rpc/ws.rs
@@ -2,6 +2,13 @@ use std::sync::mpsc;
 use jsonrpc_tcp_server::jsonrpc_core::*;
 
 pub fn register_websocket_funcs(io: &mut IoHandler, websocket: mpsc::Sender<Vec<u8>>) {
+	let send_to_websocket = move |message| -> bool {
+		match websocket.send(value.message){
+			Ok() => true,
+			Err() => false,
+		}
+	}
+
 	io.add_method("alarm_record_changes", |params: Params| {
 		#[derive(Deserialize)]
 		struct AlarmParams {
@@ -14,7 +21,6 @@ pub fn register_websocket_funcs(io: &mut IoHandler, websocket: mpsc::Sender<Vec<
 			},
 		};
 
-		websocket.send(value.message).unwrap();
-		Ok(Value::Bool(true))
+		Ok(Value::Bool(send_to_websocket(value.message)))
 	});
 }
