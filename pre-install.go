11d5338b259395177e22e9150b8eba71c9dee1fe
Tao Yang <yangtao3601@126.com>
2018-11-13 17:18:16 +0800

    fix

--- pre-install.go
+++ pre-install.go
@@ -1,19 +1,22 @@
 #!/bin/sh
 
+set -e
+
 curl https://sh.rustup.rs -sSf | sh
 echo "\n"
 source $HOME/.cargo/env
 
 # Install nightly
 rustup install nightly
-
-# registry ustc
-echo -e "[source.crates-io]\nregistry = \"https://github.com/rust-lang/crates.io-index\"\nreplace-with = 'ustc'\n" > ~/.cargo/config
-echo -e "[source.ustc]\nregistry = \"git://mirrors.ustc.edu.cn/crates.io-index\"\n" >> ~/.cargo/config
+rustup default nightly
 
 # Instarll arm tool
 sudo apt-get install -qq gcc-arm-linux-gnueabihf
 rustup target add armv7-unknown-linux-gnueabihf
+
+# registry ustc
+echo -e "[source.crates-io]\nregistry = \"https://github.com/rust-lang/crates.io-index\"\nreplace-with = 'ustc'\n" > ~/.cargo/config
+echo -e "[source.ustc]\nregistry = \"git://mirrors.ustc.edu.cn/crates.io-index\"\n" >> ~/.cargo/config
 echo -e "[target.armv7-unknown-linux-gnueabihf]\nlinker = \"arm-linux-gnueabihf-gcc\"\n" >> ~/.cargo/config
 
 # Dependency for openssl
