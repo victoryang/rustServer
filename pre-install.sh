41af6dff070c242d501bb793d6ffe55caff3ea60
Tao Yang <yangtao3601@126.com>
2018-11-14 15:45:32 +0800

    fix

--- pre-install.sh
+++ pre-install.sh
@@ -11,7 +11,7 @@
 # rustup default nightly
 
 # Instarll arm tool
-apt-get install -qq gcc-arm-linux-gnueabihf
+sudo apt-get install -qq gcc-arm-linux-gnueabihf
 rustup target add armv7-unknown-linux-gnueabihf
 
 # registry ustc
