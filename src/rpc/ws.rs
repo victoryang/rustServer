420cbef4ba2cf35db3c1addc33128110b0f18ffe
Tao Yang <yangtao3601@126.com>
2018-11-20 14:56:49 +0800

    Revert "fix"
    This reverts commit 104e65d4f6dd43558a5637771202b60811362f63.

--- src/rpc/ws.rs
+++ src/rpc/ws.rs
@@ -20,11 +20,11 @@ pub fn register_websocket_funcs(io: &mut IoHandler, websocket: mpsc::Sender<Vec<
 		match websocket_sender.lock(){
 			Ok(sender) => {
 				match sender.send(value.message){
-					Ok(()) => Ok(Value::String("true".to_string())),
-					Err(_) => Ok(Value::String("false".to_string())),
+					Ok(()) => Ok(Value::Bool(true)),
+					Err(_) => Ok(Value::Bool(false)),
 				}
 			}, 
-			Err(_) => Ok(Value::String("false".to_string())),
+			Err(_) => Ok(Value::Bool(false)),
 		}
 	});
 }
